cc1241b0d81ca2ea4482975d726402d0425ea719
sidiyes <111569840+sidiyes@users.noreply.github.com>
2022-12-30 12:07:06 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,30 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
-/* app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
-}); */
 
-/* app.use((req, res, next) => {
-  res.status(201);
-  next();
-}); */
+ mongoose.connect('mongodb+srv://usernamep6:passwordp6@cluster0.gwplgkp.mongodb.net/hotakes?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-  res.json({ message: "chhhhheehehh !" });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+}); 
 
-});
+app.use(express.json());
 
-/* app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès 2 !");
-}); */
+app.use('/api/auth', userRoutes);
+
+// app.use((req, res, next) => {
+//   res.json({ message: "test" });
+//   next();
+// });
 
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const sauceCtrl = require ('../controllers/sauce');
+
+
+
+module.exports = router;
